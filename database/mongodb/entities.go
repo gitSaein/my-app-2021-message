67df914177a6517ec6bd92d6c8fddacb5bacab8e
saeinlee <gitsaein@gmail.com>
2022-01-20 23:51:05 +0900

    mongodb _id setting

--- database/mongodb/entities.go
+++ database/mongodb/entities.go
@@ -2,12 +2,14 @@ package mongodb
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type MessageEntity struct {
-	// ID      primitive.ObjectID `bson:"_id"`
-	UserId  int `bson:"user_id"`
-	RoomId  int `bson:"room_id"`
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	UserId  int                `bson:"user_id"`
+	RoomId  int                `bson:"room_id"`
 	Message string
 	Time    time.Time
 }
